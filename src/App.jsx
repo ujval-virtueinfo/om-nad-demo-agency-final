01c4a20cc7eccc7faa061d0d14598212264749bf
Ujval <ujval99@gmail.com>
2025-10-30 23:09:01 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Services from '../components/Services'
-import Blogs from '../components/Blogs'
-import Testimonials from '../components/Testimonials'
-import Contact from '../components/Contact'
-import Footer from '../components/Footer'
-import ScrollToTop from '../components/ScrollToTop'
+import Navbar from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Blogs from './components/Blogs'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 export default function App(){
   return (
